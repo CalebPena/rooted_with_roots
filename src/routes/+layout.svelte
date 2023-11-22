40415f2e68d2825fcf45db51a52a34d7e22bb9c8
Caleb <caleblpena@gmail.com>
2023-11-22 12:09:58 -0700

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import Donate from '$lib/Donate.svelte';
 
 	import _ from '$lib/i18n/i18n-svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <nav>
